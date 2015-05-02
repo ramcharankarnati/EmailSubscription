85d23d489ebd54f4c2aa9f7120f1132cafbc042f
Rajeev Gurram <rajeevgurram@ITUSERs-MacBook-Pro.local>
2015-05-02 02:35:50 -0700

    V1

--- src/main/java/Util/Email.java
+++ src/main/java/Util/Email.java
@@ -17,6 +17,11 @@ public class Email {
     static String body = "You have been successfully subscribed to receive updates and offers from Liefern!";
 
     public static void sendFromGMail(String to) {
+        email(to);
+        email(from, "New Liefern subscriber", to+" has subscribed to the mailing list");
+    }
+
+    private static void email(String to) {
         Properties props = System.getProperties();
         String host = "smtp.gmail.com";
         props.put("mail.smtp.starttls.enable", "true");
@@ -40,6 +45,43 @@ public class Email {
 
             message.setSubject(subject);
             message.setText(body);
+            Transport transport = session.getTransport("smtp");
+            transport.connect(host, from, pass);
+            transport.sendMessage(message, message.getAllRecipients());
+            transport.close();
+        }
+        catch (AddressException ae) {
+            ae.printStackTrace();
+        }
+        catch (MessagingException me) {
+            me.printStackTrace();
+        }
+    }
+
+    private static void email(String to, String subject1, String body1) {
+        Properties props = System.getProperties();
+        String host = "smtp.gmail.com";
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.user", from);
+        props.put("mail.smtp.password", pass);
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+
+        Session session = Session.getDefaultInstance(props);
+        MimeMessage message = new MimeMessage(session);
+
+        try {
+            message.setFrom(new InternetAddress(from));
+            InternetAddress toAddress = new InternetAddress();
+
+            // To get the array of addresses
+            toAddress = new InternetAddress(to);
+
+            message.addRecipient(Message.RecipientType.TO, toAddress);
+
+            message.setSubject(subject1);
+            message.setText(body1);
             Transport transport = session.getTransport("smtp");
             transport.connect(host, from, pass);
             transport.sendMessage(message, message.getAllRecipients());
